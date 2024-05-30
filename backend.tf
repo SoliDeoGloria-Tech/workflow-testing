5f1b11c70bd45847cb913a4bc497da87d90540e4
Jerome Brown <Jerome@SoliDeoGloria.tech>
2024-05-30 15:03:39 +1200

    Add backend storage

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "github-actions-rg"
+    storage_account_name = "demostatestore"
+    container_name       = "testing"
+    key                  = "terraform.tfstate"
+    use_azuread_auth     = true
+  }
+}
