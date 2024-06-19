16fb0b30519536b5f35d8a052fe8b982e451046f
Jerome Brown <Jerome@SoliDeoGloria.tech>
2024-06-19 19:33:32 +1200

    Add another formatting issue

--- providers.tf
+++ providers.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.5"
-
+  required_version = ">= 1.7"
   required_providers {
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
-    }
   }
+}
 }
